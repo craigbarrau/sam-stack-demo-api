0d83de7e096ecba79f3e251721867f86c041fee8
Frank <wangfanjie@gmail.com>
2020-04-02 21:54:23 -0400

    Implement debug

--- libs/handler-lib.js
+++ libs/handler-lib.js
@@ -1,74 +1,65 @@
 import AWS from 'aws-sdk';
 import debug from './debug-lib';
 
+// configure AWS logger
+AWS.config.logger = debug;
+
 export function wrapper(handler) {
-  return async (event, context) =>
-    timeoutWrapper(context, () =>
-      responseWrapper(() =>
-        debugWrapper(event, () =>
-          handler(event, context)
-        )
-      )
-    );
-}
+  return async (event, context) => {
+    let timeoutTimer;
 
-async function timeoutWrapper(context, cb) {
-  // start timer
-  const timeLeft = context.getRemainingTimeInMillis();
-  let timer = setTimeout(() => {
-    if (timer) {
-      console.error('Lambda will timeout in 3 seconds');
-      debug.print();
+    // log api request data
+    debug.init();
+    debug.log('API event', JSON.stringify({
+      pathParameters: event.pathParameters,
+      queryStringParameters: event.queryStringParameters,
+      body: event.body,
+    }));
+
+    startTimeoutTimer();
+    await invokeHandler();
+    clearTimeoutTimer();
+
+    function startTimeoutTimer() {
+      const timeLeft = context.getRemainingTimeInMillis();
+      timeoutTimer = setTimeout(() => {
+        if (timer) {
+          console.error('Lambda will timeout in 3 seconds');
+          debug.print();
+        }
+      }, timeLeft - 3000);
     }
-  }, timeLeft - 3000);
 
-  await cb();
+    function clearTimeoutTimer() {
+      clearTimeout(timer);
+      timer = undefined;
+    }
 
-  // clear timer
-  clearTimeout(timer);
-  timer = undefined;
-}
+    async function invokeHandler() {
+      let statusCode = 200;
+      let responseBody;
 
-async function debugWrapper(event, cb) {
-  // configure AWS logger
-  AWS.config.logger = debug;
+      try {
+        responseBody = await handler();
+      } catch(e) {
+        // log error
+        console.error(e);
+        debug.print();
 
-  // log api request data
-  debug.init();
-  debug.log('API event', JSON.stringify({
-    pathParameters: event.pathParameters,
-    queryStringParameters: event.queryStringParameters,
-    body: event.body,
-  }));
+        statusCode = 500;
+        responseBody = { status: false, error: e.message};
+      }
 
-  try {
-    await cb();
-  } catch(e) {
-    console.error(e);
-    debug.print();
+      return {
+        statusCode,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true
+        },
+        body: JSON.stringify(responseBody)
+      };
+    }
 
-    throw e;
-  }
-}
-
-async function responseWrapper(cb) {
-  let statusCode = 200;
-  let responseBody;
-
-  try {
-    responseBody = await cb();
-  } catch(e) {
-    statusCode = 500;
-    responseBody = { status: false, error: e.message};
-  }
-
-  return {
-    statusCode,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": true
-    },
-    body: JSON.stringify(responseBody)
   };
 }
 
