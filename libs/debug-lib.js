b4264e2b57261db8c07527a6f8881e9673511b29
Frank <wangfanjie@gmail.com>
2020-04-02 22:13:51 -0400

    Implement debug

--- libs/debug-lib.js
+++ libs/debug-lib.js
@@ -1,9 +1,35 @@
+import AWS from 'aws-sdk';
 import util from 'util';
 
+AWS.config.logger = this;
+
+let timeoutTimer;
 const logs = [];
 
-export function init() {
+export function init(event, context) {
   logs.length = 0;
+
+  // Log api event
+  log('API event', JSON.stringify({
+    pathParameters: event.pathParameters,
+    queryStringParameters: event.queryStringParameters,
+    body: event.body,
+  }));
+
+  // Start timeout timer
+  const timeLeft = context.getRemainingTimeInMillis();
+  timeoutTimer = setTimeout(() => {
+    if (timeoutTimer) {
+      console.error('Lambda will timeout in 3 seconds');
+      print();
+    }
+  }, timeLeft - 3000);
+}
+
+export function end() {
+  // Clear timeout timer
+  clearTimeout(timeoutTimer);
+  timeoutTimer = undefined;
 }
 
 export function log() {
@@ -12,5 +38,8 @@ export function log() {
 }
 
 export function print() {
-  logs.forEach(({ date, string }) => console.log('DEBUG', date, string));
+  logs.forEach(({ date, string }) =>
+    console.log('DEBUG', date, string)
+  );
 }
+
--- libs/handler-lib.js
+++ libs/handler-lib.js
@@ -1,24 +1,12 @@
-import AWS from 'aws-sdk';
-import debug from './debug-lib';
-
-// configure AWS logger
-AWS.config.logger = debug;
+import * as debug from './debug-lib';
 
 export function wrapper(handler) {
   return async (event, context) => {
     let statusCode = 200;
     let responseBody;
-    let timeoutTimer;
 
-    // log api request data
-    debug.init();
-    debug.log('API event', JSON.stringify({
-      pathParameters: event.pathParameters,
-      queryStringParameters: event.queryStringParameters,
-      body: event.body,
-    }));
+    debug.init(event, context);
 
-    startTimeoutTimer();
     try {
       responseBody = await handler();
     } catch(e) {
@@ -30,7 +18,7 @@ export function wrapper(handler) {
       responseBody = { status: false, error: e.message};
     }
 
-    clearTimeoutTimer();
+    debug.end(event);
 
     return {
       statusCode,
@@ -40,21 +28,6 @@ export function wrapper(handler) {
       },
       body: JSON.stringify(responseBody)
     };
-
-    function startTimeoutTimer() {
-      const timeLeft = context.getRemainingTimeInMillis();
-      timeoutTimer = setTimeout(() => {
-        if (timer) {
-          console.error('Lambda will timeout in 3 seconds');
-          debug.print();
-        }
-      }, timeLeft - 3000);
-    }
-
-    function clearTimeoutTimer() {
-      clearTimeout(timeoutTimer);
-      timeoutTimer = undefined;
-    }
 
   };
 }
